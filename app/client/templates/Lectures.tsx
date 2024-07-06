1202370bde5ba23643497138eff183d7317d3938
Bartek Szczeciński <bartosz.szczecinski@protonmail.com>
2024-07-06 16:03:30 +0200

    Remove unnecessary div

--- app/client/templates/Lectures.tsx
+++ app/client/templates/Lectures.tsx
@@ -8,22 +8,20 @@ export const Template = (props: templates.Lectures) => {
   return (
     <Layout>
       <PageTitle>Lectures</PageTitle>
-      <div>
-        {props.lectures.map((lecture) => (
-          <Card
-            key={lecture.id}
-            title={lecture.title}
-            content={lecture.abstract}
-            authors={
-              props.all_authors_names.find(
-                (authorWithLecture) =>
-                  authorWithLecture.lecture_id === lecture.id,
-              )?.authors_names ?? ""
-            }
-            description={lecture.description ?? ""}
-          />
-        ))}
-      </div>
+      {props.lectures.map((lecture) => (
+        <Card
+          key={lecture.id}
+          title={lecture.title}
+          content={lecture.abstract}
+          authors={
+            props.all_authors_names.find(
+              (authorWithLecture) =>
+                authorWithLecture.lecture_id === lecture.id,
+            )?.authors_names ?? ""
+          }
+          description={lecture.description ?? ""}
+        />
+      ))}
     </Layout>
   );
 };
