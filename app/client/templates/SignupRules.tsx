a5b0d81aefe7a76bd84c6628f313a23cd741ee53
Bartek Szczeciński <bartosz.szczecinski@protonmail.com>
2024-07-07 21:50:40 +0200

    Refactor PrivacyPolicy, SignupRules and TermsAndConditions to use markdown

--- app/client/templates/SignupRules.tsx
+++ app/client/templates/SignupRules.tsx
@@ -1,5 +1,39 @@
 import { Layout } from "@client/components/Layout";
 import React from "react";
+import Markdown from "react-markdown";
+
+const signupRulesMarkdown = `
+## Instrukcja zapisów
+
+1. Stwórz konto za pomocą przycisku "Sign up". Jako Email podaj
+adres w domenie **cs.uni.wroc.pl** lub dowolnej innej*.  
+Konta zakładane na naszej stronie w przeszłości zostały usunięte.
+
+2. By zarejestrować się na najbliższą edycję ZOSI na stronie głównej
+lub w swoim profilu w zakładce ZOSIA kliknij przycisk **REGISTER**.
+
+3.  Uzupełnij kwestionariusz. W polu **Information** koniecznie wpisz
+swoje preferencje przy wyborze pokoju. Potrzebujemy wiedzieć czy
+wolisz pokój z łóżkiem podwójnym, a może pokój jednoosobowy, czy
+wolisz pokój w cichym zakątku hotelu, czy masz już wybraną osobę
+współlokatorską, wolisz wybrać ją później, czy może jest Ci to
+obojętne. Prosimy o uzupełnienie tego pola.
+
+4. Wyślij formularz i gotowe!
+
+Jako osoba zaproszona posiadasz specjalny priorytet i możesz wybrać
+pokój przed resztą uczestników. Do 31 grudnia mamy najwięcej
+możliwości, by pomóc Ci w wyborze pokoju. Po podanej dacie dalej
+jesteśmy do Twojej dyspozji i zawsze służymy pomocą. Natomiast im
+bliżej do daty rozpoczęcia Konferencji, tym nasz zasób pokoi będzie
+coraz bardziej ograniczony.
+
+W razie jakichkolwiek pytań lub problemów, zachęcamy do kontaktu: 
+[ksi@cs.uni.wroc.pl](mailto:ksi@cs.uni.wroc.pl)
+
+*W przypadku rejestracji konta w innej domenie prosimy o informację
+mailową. Dziękujemy.
+`;
 
 export const Template = () => {
   return (
@@ -8,45 +42,7 @@ export const Template = () => {
         <h1 className="text-center">
           Zapisy dla Zaproszonych na Zimowy Obóz Studentów Informatyki A
         </h1>
-        <h2>Instrukcja zapisów</h2>
-        <ol>
-          <li>
-            Stwórz konto za pomocą przycisku {'"Sign up"'}. Jako Email podaj
-            adres w domenie <b>cs.uni.wroc.pl</b> lub dowolnej innej*.
-            <br />
-            Konta zakładane na naszej stronie w przeszłości zostały usunięte.
-          </li>
-          <li>
-            By zarejestrować się na najbliższą edycję ZOSI na stronie głównej
-            lub w swoim profilu w zakładce ZOSIA kliknij przycisk{" "}
-            <b>REGISTER</b>.
-          </li>
-          <li>
-            Uzupełnij kwestionariusz. W polu <b>Information</b> koniecznie wpisz
-            swoje preferencje przy wyborze pokoju. Potrzebujemy wiedzieć czy
-            wolisz pokój z łóżkiem podwójnym, a może pokój jednoosobowy, czy
-            wolisz pokój w cichym zakątku hotelu, czy masz już wybraną osobę
-            współlokatorską, wolisz wybrać ją później, czy może jest Ci to
-            obojętne. Prosimy o uzupełnienie tego pola.
-          </li>
-          <li>Wyślij formularz i gotowe!</li>
-        </ol>
-        <p>
-          Jako osoba zaproszona posiadasz specjalny priorytet i możesz wybrać
-          pokój przed resztą uczestników. Do 31 grudnia mamy najwięcej
-          możliwości, by pomóc Ci w wyborze pokoju. Po podanej dacie dalej
-          jesteśmy do Twojej dyspozji i zawsze służymy pomocą. Natomiast im
-          bliżej do daty rozpoczęcia Konferencji, tym nasz zasób pokoi będzie
-          coraz bardziej ograniczony.
-        </p>
-        <p>
-          W razie jakichkolwiek pytań lub problemów, zachęcamy do kontaktu:{" "}
-          <a href="mailto:ksi@cs.uni.wroc.pl">ksi@cs.uni.wroc.pl</a>.
-        </p>
-        <p>
-          * W przypadku rejestracji konta w innej domenie prosimy o informację
-          mailową. Dziękujemy.
-        </p>
+        <Markdown>{signupRulesMarkdown}</Markdown>
       </article>
     </Layout>
   );
--- app/client/templates/TermsAndConditions.tsx
+++ app/client/templates/TermsAndConditions.tsx
@@ -2,6 +2,106 @@ import { Layout } from "@client/components/Layout";
 import { getLocalDate } from "@client/utils/time";
 import { templates } from "@reactivated";
 import React from "react";
+import Markdown from "react-markdown";
+
+const privacyPolicyMarkdown = (
+  zosiaTitle: string,
+  town: string,
+  startDate: string,
+  endDate: string,
+) => `
+1. Niniejszy regulamin reguluje zasady obowiązujące osoby
+uczestniczące w Ogólnopolskiej Konferencji „${zosiaTitle}”,
+zwanej dalej Konferencją, odbywającej się w miejscowości
+${town}, w dniach ${startDate} - ${endDate}, a także
+definiuje konsekwencje wynikające z nieprzestrzegania tych zasad.
+
+2. Organizatorem Konferencji jest Koło Studentów Informatyki
+działające przy Wydziale Matematyki i Informatyki Uniwersytetu
+Wrocławskiego, z siedzibą w Instytucie Informatyki Uniwersytetu
+Wrocławskiego przy ul. Joliot-Curie 15, 50-383 Wrocław.
+
+3. Osobami uczestniczącymi w Konferencji, zwanymi dalej
+Uczestnikami, mogą być jedynie osoby zarejestrowane w serwisie
+internetowym Konferencji, które uiściły odpowiednią opłatę
+wyliczoną indywidualnie dla każdego Uczestnika na podstawie
+podanych przez niego danych.
+
+4. Konferencja rozpoczyna się dnia ${startDate} i kończy dnia
+${endDate}, jednakże czas pobytu każdego z Uczestników jest
+zależny od jego preferencji zadeklarowanych w systemie
+internetowym.
+
+5. W trakcie trwania Konferencji, Uczestnik zobowiązany jest do
+stosowania się do poleceń Organizatora i osób przez niego
+wyznaczonych.
+
+6. Nieodpowiednie zachowanie Uczestnika w trakcie Konferencji, w
+szczególności niestosowanie się do poleceń osób wymienionych w
+punkcie 5, nadużywanie alkoholu, stosowanie środków
+odurzających, bądź inne zachowania nieobyczajne, a także
+zachowania stwarzające zagrożenie zdrowia i bezpieczeństwa
+Uczestnika bądź innych Uczestników stanowią podstawę do
+zastosowania następujących środków dyscyplinujących:
+
+    1. upomnienia;
+    2. powiadomienia władz organizacji macierzystej (uczelni) o
+       zachowaniu Uczestnika;
+    3. wydalenia z Konferencji.    
+
+    O zastosowaniu środka dyscyplinującego, w tym także o rodzaju
+    zastosowanego środka, decyduje jednoosobowo osoba wskazana przez
+    Organizatora jako Kierownik Konferencji. Kierownik Konferencji
+    może zdecydować o nałożeniu środka najbardziej dotkliwego bez
+    uprzedniego stosowania pozostałych środków. Kierownik
+    Konferencji może zdecydować o zastosowaniu wszystkich środków
+    dyscyplinujących łącznie. Władze uczelni macierzystych względem
+    osób będących Uczestnikami mogą za to samo zachowanie zastosować
+    kary dyscyplinarne wynikające z Regulaminu Studiów.
+
+7. Na terenie całego ośrodka, w którym odbywać się będzie
+Konferencja obowiązuje bezwzględny zakaz palenia papierosów oraz
+innych wyrobów nikotynowych (w tym papierosów elektronicznych,
+itp.). W przypadku nieprzestrzegania tego zakazu organizator
+zastrzega sobie prawo do zastosowania środków dyscyplinujących
+opisanych w punkcie 6.
+
+8. Uczestnik ponosi odpowiedzialność za szkody wyrządzone osobom
+trzecim w czasie przebywania na Konferencji. Organizator nie
+przejmuje odpowiedzialności względem osób trzecich za zachowania
+Uczestnika w trakcie Konferencji.
+
+9. Organizator nie sprawuje pieczy nad osobą Uczestnika oraz jego
+mieniem w trakcie Konferencji. W szczególności Organizator nie
+ponosi odpowiedzialności za mienie Uczestnika pozostawione w
+obiekcie, w którym organizowana jest Konferencja.
+
+10. Uczestnik podający w serwisie internetowym nieprawdziwe dane,
+zgadza się na konsekwencje prawne i finansowe z tego wynikające.
+Organizator zastrzega sobie prawo do wydalenia takiego
+Uczestnika z Konferencji. W przypadku, gdy informacje podane
+przez Uczestnika narażają Organizatorów na straty finansowe,
+Uczestnik zobowiązuje się do zwrotu poniesionych przez
+Organizatora dodatkowych kosztów.
+
+11. Uczestnikowi nie przysługuje zwrot jego świadczenia na rzecz
+Organizatora bądź części tego świadczenia w przypadku niepełnego
+udziału w Konferencji z przyczyn, za które Organizator nie
+odpowiada. W szczególności zwrot świadczenia bądź części
+świadczenia nie przysługuje Uczestnikowi, względem którego
+zastosowano karę wydalenia z Konferencji.
+
+12. Warunki rezygnacji:
+
+    1. Rezygnacja z Konferencji następuje w momencie złożenia przez
+    Uczestnika rezygnacji w formie pisemnej lub elektronicznej
+    na adres pocztowy Organizatora.
+
+    2. W przypadku rezygnacji potrąca się 100% wpłaconej kwoty.
+
+    3. Istnieje możliwość zwrotu wpłaconej kwoty, gdy rezygnujący
+    Uczestnik znajdzie na to miejsce innego Uczestnika.
+`;
 
 export const Template = (props: templates.TermsAndConditions) => {
   let startDate = "";
@@ -19,117 +119,14 @@ export const Template = (props: templates.TermsAndConditions) => {
           Regulamin Zimowego Obozu Studentów Informatyki A
         </h1>
         {props.zosia ? (
-          <ol>
-            <li>
-              Niniejszy regulamin reguluje zasady obowiązujące osoby
-              uczestniczące w Ogólnopolskiej Konferencji „{props.zosia_title}”,
-              zwanej dalej Konferencją, odbywającej się w miejscowości{" "}
-              {props.place.town}, w dniach {startDate} - {endDate}, a także
-              definiuje konsekwencje wynikające z nieprzestrzegania tych zasad.
-            </li>
-            <li>
-              Organizatorem Konferencji jest Koło Studentów Informatyki
-              działające przy Wydziale Matematyki i Informatyki Uniwersytetu
-              Wrocławskiego, z siedzibą w Instytucie Informatyki Uniwersytetu
-              Wrocławskiego przy ul. Joliot-Curie 15, 50-383 Wrocław.
-            </li>
-            <li>
-              Osobami uczestniczącymi w Konferencji, zwanymi dalej Uczestnikami,
-              mogą być jedynie osoby zarejestrowane w serwisie internetowym
-              Konferencji, które uiściły odpowiednią opłatę wyliczoną
-              indywidualnie dla każdego Uczestnika na podstawie podanych przez
-              niego danych.
-            </li>
-            <li>
-              Konferencja rozpoczyna się dnia {startDate} i kończy dnia{" "}
-              {endDate}, jednakże czas pobytu każdego z Uczestników jest zależny
-              od jego preferencji zadeklarowanych w systemie internetowym.
-            </li>
-            <li>
-              W trakcie trwania Konferencji, Uczestnik zobowiązany jest do
-              stosowania się do poleceń Organizatora i osób przez niego
-              wyznaczonych.
-            </li>
-            <li>
-              Nieodpowiednie zachowanie Uczestnika w trakcie Konferencji, w
-              szczególności niestosowanie się do poleceń osób wymienionych w
-              punkcie 5, nadużywanie alkoholu, stosowanie środków odurzających,
-              bądź inne zachowania nieobyczajne, a także zachowania stwarzające
-              zagrożenie zdrowia i bezpieczeństwa Uczestnika bądź innych
-              Uczestników stanowią podstawę do zastosowania następujących
-              środków dyscyplinujących:
-              <ol>
-                <li>upomnienia;</li>
-                <li>
-                  powiadomienia władz organizacji macierzystej (uczelni) o
-                  zachowaniu Uczestnika;
-                </li>
-                <li>wydalenia z Konferencji.</li>
-              </ol>
-              O zastosowaniu środka dyscyplinującego, w tym także o rodzaju
-              zastosowanego środka, decyduje jednoosobowo osoba wskazana przez
-              Organizatora jako Kierownik Konferencji. Kierownik Konferencji
-              może zdecydować o nałożeniu środka najbardziej dotkliwego bez
-              uprzedniego stosowania pozostałych środków. Kierownik Konferencji
-              może zdecydować o zastosowaniu wszystkich środków dyscyplinujących
-              łącznie. Władze uczelni macierzystych względem osób będących
-              Uczestnikami mogą za to samo zachowanie zastosować kary
-              dyscyplinarne wynikające z Regulaminu Studiów.
-            </li>
-            <li>
-              Na terenie całego ośrodka, w którym odbywać się będzie Konferencja
-              obowiązuje bezwzględny zakaz palenia papierosów oraz innych
-              wyrobów nikotynowych (w tym papierosów elektronicznych, itp.). W
-              przypadku nieprzestrzegania tego zakazu organizator zastrzega
-              sobie prawo do zastosowania środków dyscyplinujących opisanych w
-              punkcie 6.
-            </li>
-            <li>
-              Uczestnik ponosi odpowiedzialność za szkody wyrządzone osobom
-              trzecim w czasie przebywania na Konferencji. Organizator nie
-              przejmuje odpowiedzialności względem osób trzecich za zachowania
-              Uczestnika w trakcie Konferencji.
-            </li>
-            <li>
-              Organizator nie sprawuje pieczy nad osobą Uczestnika oraz jego
-              mieniem w trakcie Konferencji. W szczególności Organizator nie
-              ponosi odpowiedzialności za mienie Uczestnika pozostawione w
-              obiekcie, w którym organizowana jest Konferencja.
-            </li>
-            <li>
-              Uczestnik podający w serwisie internetowym nieprawdziwe dane,
-              zgadza się na konsekwencje prawne i finansowe z tego wynikające.
-              Organizator zastrzega sobie prawo do wydalenia takiego Uczestnika
-              z Konferencji. W przypadku, gdy informacje podane przez Uczestnika
-              narażają Organizatorów na straty finansowe, Uczestnik zobowiązuje
-              się do zwrotu poniesionych przez Organizatora dodatkowych kosztów.
-            </li>
-            <li>
-              Uczestnikowi nie przysługuje zwrot jego świadczenia na rzecz
-              Organizatora bądź części tego świadczenia w przypadku niepełnego
-              udziału w Konferencji z przyczyn, za które Organizator nie
-              odpowiada. W szczególności zwrot świadczenia bądź części
-              świadczenia nie przysługuje Uczestnikowi, względem którego
-              zastosowano karę wydalenia z Konferencji.
-            </li>
-            <li>
-              Warunki rezygnacji:
-              <ol>
-                <li>
-                  Rezygnacja z Konferencji następuje w momencie złożenia przez
-                  Uczestnika rezygnacji w formie pisemnej lub elektronicznej na
-                  adres pocztowy Organizatora.
-                </li>
-                <li>
-                  W przypadku rezygnacji potrąca się 100% wpłaconej kwoty.
-                </li>
-                <li>
-                  Istnieje możliwość zwrotu wpłaconej kwoty, gdy rezygnujący
-                  Uczestnik znajdzie na to miejsce innego Uczestnika.
-                </li>
-              </ol>
-            </li>
-          </ol>
+          <Markdown>
+            {privacyPolicyMarkdown(
+              props.zosia_title,
+              props.place.town,
+              startDate,
+              endDate,
+            )}
+          </Markdown>
         ) : (
           <h2 className="text-center">W przygotowaniu...</h2>
         )}
