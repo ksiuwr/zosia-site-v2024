cc25cc8e3c89822dbbad5077cb939600b41732c8
Bartek Szczeciński <bartosz.szczecinski@protonmail.com>
2024-07-22 20:15:09 +0200

    Refactor BasicForm field type conversion

--- app/client/components/forms/BasicForm.tsx
+++ app/client/components/forms/BasicForm.tsx
@@ -25,9 +25,11 @@ export const BasicForm = <T extends FieldMap>({
       {form.visibleFields.map((field) => (
         <BasicFormField
           key={field.name}
-          // @ts-expect-error Typescript complains about this conversion, but it can't be avoided without changing types exposed by Reactivated.
-          // The conversion is safe, because `field`'s type is more specific than FieldHandler.
-          field={field as FieldHandler}
+          // Typescript complains about this conversion, but it can't be avoided without changing types exposed by Reactivated.
+          // The conversion is safe, because field's type is more specific than FieldHandler.
+          // This approach allows us to automatically render all the fields defined in a Django form.
+          // So for example if you add a field to a Django form, no frontend changes will be required.
+          field={field as unknown as FieldHandler}
         />
       ))}
       <button type="submit" className="btn btn-primary btn-lg my-4 w-full">
