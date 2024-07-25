d7fb06c72f331006b024b432070e525b0cbe007e
Bartek Szczeciński <bartosz.szczecinski@protonmail.com>
2024-07-26 00:30:58 +0200

    Add clsx to BasicFormField

--- app/client/components/forms/BasicFormField.tsx
+++ app/client/components/forms/BasicFormField.tsx
@@ -1,5 +1,6 @@
 import { Field } from "@headlessui/react";
 import { FieldHandler } from "@reactivated";
+import clsx from "clsx";
 import React from "react";
 import { BasicDescription } from "./BasicDescription";
 import { BasicLabel } from "./BasicLabel";
@@ -18,17 +19,19 @@ export const BasicFormField = ({
   checked = undefined,
   onCheckboxChange = undefined,
 }: BasicFormFieldProps) => {
+  const isCheckbox = field.tag === "django.forms.widgets.CheckboxInput";
+
   return (
     <Field
       className="mb-4 flex flex-col"
       disabled={disabled === undefined ? field.disabled : disabled}
     >
       <div
-        className={`flex ${
-          field.tag === "django.forms.widgets.CheckboxInput"
-            ? "flex-row items-center gap-x-2"
-            : "flex-col"
-        }`}
+        className={clsx(
+          "flex",
+          isCheckbox && "flex-row items-center gap-x-2",
+          !isCheckbox && "flex-col",
+        )}
       >
         <BasicLabel field={field} />
         <BasicWidget
