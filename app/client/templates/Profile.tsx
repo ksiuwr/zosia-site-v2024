53fa663fb4481709dd999e03ce03d3853f7f614c
Bartek Szczeciński <bartosz.szczecinski@protonmail.com>
2024-08-02 03:15:26 +0200

    Add "Admin Panel" button to Profile page

--- app/client/templates/Profile.tsx
+++ app/client/templates/Profile.tsx
@@ -1,11 +1,14 @@
+import { Alert } from "@client/components/alert/Alert";
 import { CenteredContentContainer } from "@client/components/containers/CenteredContentContainer";
 import { Layout } from "@client/components/Layout";
 import {
   BookOpenIcon,
   HomeModernIcon,
+  LockClosedIcon,
   PuzzlePieceIcon,
 } from "@heroicons/react/24/solid";
 import { Context, reverse, templates } from "@reactivated";
+import clsx from "clsx";
 import React, { useContext } from "react";
 
 const ICON_SIZE = 8;
@@ -16,29 +19,47 @@ export const Template = (props: templates.Profile) => {
   return (
     <Layout>
       <CenteredContentContainer>
-        <div className="join my-8 flex justify-evenly lg:flex-row">
-          <a
-            className="btn btn-outline join-item h-fit grow flex-col py-4"
-            href={reverse("boardgames_index")}
-          >
-            <PuzzlePieceIcon className={`size-${ICON_SIZE}`} />
-            Boardgames
-          </a>
-          <a
-            className="btn btn-outline join-item h-fit grow flex-col px-8 py-4"
-            href={reverse("rooms_index")}
-          >
-            <HomeModernIcon className={`size-${ICON_SIZE}`} />
-            Rooms
-          </a>
-          <a
-            className="btn btn-outline join-item h-fit grow flex-col py-4"
-            href={reverse("lectures_add")}
-          >
-            <BookOpenIcon className={`size-${ICON_SIZE}`} />
-            Add lecture
-          </a>
+        <div className="my-6">
+          <div className="join flex justify-evenly lg:flex-row">
+            <a
+              className={clsx(
+                "btn btn-outline join-item h-fit grow flex-col py-4",
+                user.is_staff && "rounded-bl-none",
+              )}
+              href={reverse("boardgames_index")}
+            >
+              <PuzzlePieceIcon className={`size-${ICON_SIZE}`} />
+              Boardgames
+            </a>
+            <a
+              className="btn btn-outline join-item h-fit grow flex-col px-8 py-4"
+              href={reverse("rooms_index")}
+            >
+              <HomeModernIcon className={`size-${ICON_SIZE}`} />
+              Rooms
+            </a>
+            <a
+              className={clsx(
+                "btn btn-outline join-item h-fit grow flex-col py-4",
+                user.is_staff && "rounded-br-none",
+              )}
+              href={reverse("lectures_add")}
+            >
+              <BookOpenIcon className={`size-${ICON_SIZE}`} />
+              Add lecture
+            </a>
+          </div>
+          {user.is_staff && (
+            <a
+              className="btn btn-outline btn-block h-fit rounded-t-none border-t-0 py-4"
+              href={reverse("admin")}
+            >
+              <LockClosedIcon className={`size-${ICON_SIZE}`} />
+              Admin panel
+            </a>
+          )}
         </div>
+
         <div className="card card-compact mb-8 bg-base-300 lg:card-normal">
           <div className="card-body">
             <h2 className="card-title text-lg lg:text-xl">Payments</h2>
@@ -66,6 +87,15 @@ export const Template = (props: templates.Profile) => {
               <span className="font-bold">Email:</span> {user.email}
             </p>
 
+            {user.is_staff && (
+              <Alert type="warning">
+                Your account has admin access! Check out the{" "}
+                <a className="link" href={reverse("admin")}>
+                  admin panel
+                </a>
+                .
+              </Alert>
+            )}
             <a className="btn btn-outline" href={reverse("accounts_edit")}>
               Edit name
             </a>
