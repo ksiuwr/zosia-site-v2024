c70be030105d9f1a295c6d9a1da92f8509d39d39
Bartek Szczeciński <bartosz.szczecinski@protonmail.com>
2024-11-04 13:03:17 +0100

    Fix wrong bottom margin in navbar

--- app/client/components/admin/layout/AdminLayout.tsx
+++ app/client/components/admin/layout/AdminLayout.tsx
@@ -15,7 +15,7 @@ export const AdminLayout = ({ children }: PropsWithChildren) => {
           <ArrowLeftIcon className="size-6" /> Back to admin panel
         </a>
       </AdminCenteredContainer>
-      {children}
+      <div className="mb-4 lg:mb-8">{children}</div>
     </Layout>
   );
 };
