0c69c6f8765df7eb50ad2ad2a5508224389e754f
Bartek Szczeciński <bartosz.szczecinski@protonmail.com>
2024-11-19 22:59:45 +0100

    Add echo messages to deploy script

--- app/deploy.sh
+++ app/deploy.sh
@@ -14,23 +14,25 @@ IMAGE_NAME=zosia_prod
 
 IMAGE_URL=$REPO_HOSTNAME/$PROJECT_ID/$REPO_NAME/$IMAGE_NAME:latest
 
-# Configure gcloud and docker to be able to push to the Google Container Registry
+echo "[1] Configuring gcloud and docker to be able to push to the Google Container Registry"
 gcloud config set project $PROJECT_ID
 gcloud config set compute/zone $REGION
 gcloud auth configure-docker $REPO_HOSTNAME
 
-# Build and push the image
+echo "[2] Building and pushing the image"
 docker build --target prod -t $IMAGE_URL .
 docker push $IMAGE_URL
 
-# Run the migration job
+echo "[3] Running migrations"
 gcloud run jobs update migrate --region=$REGION --image $IMAGE_URL
 gcloud run jobs execute migrate --wait --region=$REGION
 
 # Run the collectstatic job which will collect all the static files into GCS bucket
+echo "[4] Collecting static files"
 gcloud run jobs update collectstatic --region=$REGION --image $IMAGE_URL
 gcloud run jobs execute collectstatic --wait --region=$REGION
 
 # Deploy new service revision with the new image
+echo "[5] Deploying the new image"
 gcloud run services update zosia --region $REGION --image $IMAGE_URL
 
