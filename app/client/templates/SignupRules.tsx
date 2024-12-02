aa63882e2772e3728d70f58f63acc289d160e542
Michał Hajahmadov <michal.hajahmadov@tensquaregames.com>
2024-12-02 22:29:42 +0100

    Revert "Update sign-up info (revert me later)"
    This reverts commit 6424f05899c702b58f2cce91e61f386a1edb5d10.

--- app/client/templates/SignupRules.tsx
+++ app/client/templates/SignupRules.tsx
@@ -6,13 +6,28 @@ import Markdown from "react-markdown";
 const signupRulesMarkdown = `
 ## Instrukcja zapisów
 
-Stwórz konto za pomocą przycisku "Sign up". Jako Email podaj
+1. Stwórz konto za pomocą przycisku "Sign up". Jako Email podaj
 adres w domenie **cs.uni.wroc.pl** lub dowolnej innej*.  
 Konta zakładane na naszej stronie w przeszłości zostały usunięte.
 
+2. By zarejestrować się na najbliższą edycję ZOSI na stronie głównej
+lub w swoim profilu w zakładce ZOSIA kliknij przycisk **REGISTER**.
+
+3.  Uzupełnij kwestionariusz. W polu **Information** koniecznie wpisz
+swoje preferencje przy wyborze pokoju. Potrzebujemy wiedzieć czy
+wolisz pokój z łóżkiem podwójnym, a może pokój jednoosobowy, czy
+wolisz pokój w cichym zakątku hotelu, czy masz już wybraną osobę
+współlokatorską, wolisz wybrać ją później, czy może jest Ci to
+obojętne. Prosimy o uzupełnienie tego pola.
+
+4. Wyślij formularz i gotowe!
+
 Jako osoba zaproszona posiadasz specjalny priorytet i możesz wybrać
-pokój przed resztą uczestników. Wcześniejsze zapisy startują
-na początku grudnia. O ich początku poinformujemy mailowo.
+pokój przed resztą uczestników. Do 31 grudnia mamy najwięcej
+możliwości, by pomóc Ci w wyborze pokoju. Po podanej dacie dalej
+jesteśmy do Twojej dyspozji i zawsze służymy pomocą. Natomiast im
+bliżej do daty rozpoczęcia Konferencji, tym nasz zasób pokoi będzie
+coraz bardziej ograniczony.
 
 W razie jakichkolwiek pytań lub problemów, zachęcamy do kontaktu: 
 [ksi@cs.uni.wroc.pl](mailto:ksi@cs.uni.wroc.pl)
