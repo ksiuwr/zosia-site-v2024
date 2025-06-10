d101e3881407a8950ca3d2492d6aacb048c240d1
weebik <krystian.samaa@gmail.com>
2025-06-10 14:21:42 +0200

    chore: changed themes, logo and text

--- app/client/components/homePage/About.tsx
+++ app/client/components/homePage/About.tsx
@@ -7,16 +7,14 @@ export const About = () => {
       <div className="card card-compact my-8 bg-base-300 lg:card-normal lg:my-14">
         <div className="card-body text-base-content lg:text-lg">
           <p>
-            <b>ZOSIA</b>{" "}
-            {`("Zimowy Obóz Studentów Informatyki A", roughly
-          translated as "The Best Computer Science Students' Winter Camp") is
-          the annual gathering for people associated with the Institute of
-          Computer Science at the University of Wroclaw. It started 20 years ago
-          with a tight-knit group of friends and has grown to group of about 200
-          people from all over the world. During the three-day conference
-          attendees are welcome not only to enjoy talks (not only about computer
-          science), but also various outdoor activities, trips, and of course
-          integration with past and current students and lecturers.`}
+            <b>LEOSIA</b>{" "}
+            {`(“Letni Obóz Studentów Informatyki A”; “Computer Science Students’ Summer Camp”)
+             is an event inspired by the long-standing winter conference ZOSIA, which has been
+              traditionally held for the past 20 years at our institute. For now a smaller and
+               more informal version of a scientific conference, LEOSIA will take place for the
+              first time this summer. During the 3-day conference, participants will not only
+              enjoy talks and lectures, but also outdoor activities, trips, and the opportunity
+              to socialize with both current and former students and lecturers.`}
           </p>
         </div>
       </div>
