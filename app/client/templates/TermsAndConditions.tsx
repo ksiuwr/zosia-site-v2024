58ffb7fa3f27337662197d0f6111d3b9ad8dc6b2
Wojciech Antoni Kieloch <94644742+wak-sudo@users.noreply.github.com>
2025-08-24 21:46:42 +0200

    Change terms and conditions

--- app/client/templates/TermsAndConditions.tsx
+++ app/client/templates/TermsAndConditions.tsx
@@ -99,7 +99,7 @@ Organizator może także odmówić świadczenia usług tej osobie w przyszłośc
 Organizator zastrzega sobie prawo do odmowy realizacji konkretnej usługi na **7 dni** przed rozpoczęciem obozu z istotnych przyczyn organizacyjnych lub technicznych. Uczestnik zostanie o tym niezwłocznie poinformowany, a wniesiona opłata za tę usługę zostanie zwrócona w terminie 7 dni.
 
 ### 16. Odwołanie Obozu
-W przypadku nieuzbierania wystarczającej ilości środków niezbędnych do przeprowadzenia Obozu organizator zastrzega sobie prawo do odwołania wydarzenia. Decyzja w tej sprawie może zostać podjęta i opublikowana najpóźniej do dnia 24 sierpnia 2025 r. (włącznie). Wpłacone środki zostaną zwrócone uczestnikom w ciągu 7 dni od momentu ogłoszenia decyzji.
+W przypadku nieuzbierania wystarczającej ilości środków niezbędnych do przeprowadzenia Obozu organizator zastrzega sobie prawo do odwołania wydarzenia. Decyzja w tej sprawie może zostać podjęta i opublikowana najpóźniej do dnia 2 września 2025 r. (włącznie). Wpłacone środki zostaną zwrócone uczestnikom w ciągu 7 dni od momentu ogłoszenia decyzji.
 
 ---
 
